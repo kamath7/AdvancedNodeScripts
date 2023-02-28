24e0dd056639bd842fe52da819b982119bf9448f
kamath7 <adithyakamath96@gmail.com>
2023-02-28 17:47:20 +0530

    cluster fork

--- Express/app.js
+++ Express/app.js
@@ -2,18 +2,23 @@ const express = require("express");
 const cluster = require("cluster");
 
 console.log(cluster.isMaster); //true.
-const app = express();
 
-function playingWithCPU(duration) {
-  const start = Date.now();
-  while (Date.now() - start < duration) {
-    //infinite loop
+if (cluster.isMaster) {
+  cluster.fork();
+  //will execute index.js in child mode
+} else {
+  const app = express();
+  function playingWithCPU(duration) {
+    const start = Date.now();
+    while (Date.now() - start < duration) {
+      //infinite loop
+    }
   }
+
+  app.get("/", (req, res) => {
+    playingWithCPU(5000);
+    res.send("hi");
+  });
+
+  app.listen(3000);
 }
-
-app.get("/", (req, res) => {
-  playingWithCPU(5000);
-  res.send("hi");
-});
-
-app.listen(3000);

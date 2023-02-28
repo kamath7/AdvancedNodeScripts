ef94bbb15aabb36b400e9a5f084d46af12ce281e
kamath7 <adithyakamath96@gmail.com>
2023-02-28 17:33:34 +0530

    creating an useless function to waste time

--- Express/app.js
+++ Express/app.js
@@ -2,7 +2,15 @@ const express = require('express')
 
 const app = express()
 
+function playingWithCPU(duration){
+    const start = Date.now()
+    while(Date.now() - start < duration){
+        //infinite loop
+    }
+}
+
 app.get("/",(req,res) =>{
+    playingWithCPU(5000)
     res.send("hi")
 })
 

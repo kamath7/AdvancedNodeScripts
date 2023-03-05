4e1c32f9d065bb68b2610100a0c572ddceeb3d1d
kamath7 <adithyakamath96@gmail.com>
2023-03-05 21:45:53 +0530

    onmessage and postMessage

--- WorkerThreads/app.js
+++ WorkerThreads/app.js
@@ -1,17 +1,17 @@
-const express = require('express')
-const crypto = require('crypto')
-const app = express()
+const express = require("express");
+const crypto = require("crypto");
+const app = express();
 
-const Worker = require('webworker-threads')
+const Worker = require("webworker-threads");
 
 app.get("/", (req, res) => {
-    crypto.pbkdf2("a", "b", 100000, 512, "sha512", () => {
-      res.send("hi");
-    });
-  });
+  const worker = new Worker(function () {});
+  worker.onmessage = function () {};
+  worker.postMessage();
+});
 
-  app.get("/better", (req, res) => {
-    res.send("hi");
-  });
+app.get("/better", (req, res) => {
+  res.send("hi");
+});
 
-  app.listen(3000);
+app.listen(3000);
